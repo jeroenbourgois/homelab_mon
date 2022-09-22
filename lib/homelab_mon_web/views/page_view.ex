1a8d8220e8105f700ffe7dd06d2554a8f64b7858
Jeroen Bourgois <jeroen@jackjoe.be>
2022-09-22 21:12:00 +0200

    feat: last update output

--- lib/homelab_mon_web/views/page_view.ex
+++ lib/homelab_mon_web/views/page_view.ex
@@ -1,3 +1,9 @@
 defmodule HomelabMonWeb.PageView do
   use HomelabMonWeb, :view
+
+  def solar_edge_last_updated(%{"updated_at" => updated_at}) do
+    Calendar.strftime(updated_at, "%d-%m-%Y %H:%M")
+  end
+
+  def solar_edge_last_updated(_), do: "n/a"
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HomelabMon.MixProject do
   def project do
     [
       app: :homelab_mon,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
